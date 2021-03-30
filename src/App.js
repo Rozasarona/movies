13cee7daa5beda33948d29e1e7246a4a314c15d3
Svetlana M <svetlanam300@gmail.com>
2021-03-30 12:23:49 +0300

    list received

--- src/App.js
+++ src/App.js
@@ -14,27 +14,26 @@ class App extends React.Component {
     }
     componentDidMount() {
         fetch("https://api.themoviedb.org/3/search/movie?api_key=2174bad4d702278c7b79c6172f192382&language=en-US&query=return&page=1&include_adult=false")
-            .then(res => res.json())
+           .then(res => res.json())
             .then(
                (result) => {
                    this.setState({
-                       films: result.films
+                       films: result.results
                    });
                    console.log(result);
-               },
-               
-            )         
+                }, 
+            )   
+                  
      }
     
     render()  {
         
-       /* for(let i = 0; i<=5; i++) {
-
-        }*/
+       
 
         return(
             <FilmList 
                 films = {this.state.films}/>
+                
         )    
     }
 }
