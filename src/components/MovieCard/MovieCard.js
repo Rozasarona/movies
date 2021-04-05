f96af7ef44ca9a221ded10d74acdc360a39c05b5
Svetlana M <svetlanam300@gmail.com>
2021-04-05 16:48:30 +0300

    Revert "moviecard class"
    This reverts commit 516c5a64f7af924ccf9f74df7519ba88aa39f7c2.

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -3,61 +3,55 @@ import './MovieCard.css';
 import FilmCover from '../FilmCover/FilmCover';
 import { format } from 'date-fns';
 import { Spin } from 'antd';
-import { render } from '@testing-library/react';
 
 
 
-class MovieCard extends React.Component {
+function MovieCard ({poster_path, title, overview, configuration, release_date, loading}) {
 
-    constructor (props) {
-        super(props);
-        this.state = {
-            loading: true
-        };
-    }
-
-    cutOverview = () => {
-        const { overview, maxLength } = this.props;
+    function cutOverview (overview, maxLength) {
         if (overview.length <= maxLength) return overview;
        
         for(let i=maxLength; i >=0; i--) {
             if(overview[i] === ' ') return overview.slice(0, i) + '...';
         }
     }
-    
-   
-         
-        render() {
-            const { poster_path, title, overview, configuration, release_date } = this.props
-            let releaseDate = new Date(release_date);            
-            return (
-                <div className="moviesapp_film">                    
-                    <FilmCover
-                        configuration = { configuration } 
-                        poster_path = { poster_path } />
-                    <div className="moviesapp_content">
-                        <h2>{title}</h2>
-                        <span className="filmDate">{ format(releaseDate, 'MMMM d, yyyy') }</span><br/><br/>
-                        <button>Action</button>&nbsp;
-                        <button>Drama</button><br/><br/>
-                        <p>{ this.cutOverview(overview, 130) }</p>
-                    </div>
-                    
-                </div>
-            );
-        };
+
+    let releaseDate = new Date(release_date);
+
+    if (loading) {
+        return <Spin size="large"/>
+    }
+
+    const spinner = loading ? <Spin /> : null;
+    const content != loading ? <FilmCardView /> : null
+
+    return (
+        <div className="moviesapp_film">
+            { spinner }
+            { content }
+        </div>
+    );
 }
 
 export default MovieCard;
 
 
+const FilmCardView = () => {
+    return (
+       <React.Fragment>
+           <FilmCover
+               configuration = { configuration } 
+               poster_path = { poster_path } />
+            <div className="moviesapp_content">
+                <h2>{title}</h2>
+                <span className="filmDate">{ format(releaseDate, 'MMMM d, yyyy') }</span><br/><br/>
+                <button>Action</button>&nbsp;
+                <button>Drama</button><br/><br/>
+                <p>{ cutOverview(overview, 130) }</p>
+            </div>
+       </React.Fragment> 
+    )
+}
 
-/* const FilmCardView = ({film}) => {
-    const { poster_path, title, overview, 
-            configuration, release_date} = film; 
-            
-            const spinner = loading ? <Spin size = "large" /> : null;
-    const content = !loading ? <FilmCardView film = {film} /> : null */
     
-
-    //({poster_path, title, overview, configuration, release_date, loading})
+    
