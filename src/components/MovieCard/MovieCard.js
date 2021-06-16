0447060778a78262fa3f12d1ba6fb1bdd5583c2a
Svetlana Mamadzhanova <svetlanam300@gmail.com>
2021-06-16 23:10:01 +0300

    film/Cover delete

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { format } from 'date-fns';
-import { Rate } from 'antd';
+import 'antd/dist/antd.css';
+import { Rate, Image } from 'antd';
 
-import FilmCover from '../FilmCover/FilmCover';
+
 import AverageRating from '../AverageRating/AverageRating';
 
 import { GenresContext, ConfigurationContext } from '../ReactContexts';
@@ -31,12 +32,18 @@ function MovieCard ({id, poster_path, title, overview, release_date, genre_ids, 
                 <ConfigurationContext.Consumer>
                     {configuration => {
                         const genres = genre_ids.map(genre_id => allGenres.find(genre => genre.id === genre_id));
-
+                        const latest_poster_size_index = configuration.images.poster_sizes.length - 1;
+                        const poster_size_original = configuration.images.poster_sizes[latest_poster_size_index];
+                        const url = poster_path
+                        ? `${configuration.images.base_url}${poster_size_original}${poster_path}`
+                        : 'https://via.placeholder.com/183x281.png?text=No+Cover';
                         return (
                             <div className="moviesapp_film">
-                                <FilmCover
-                                    configuration={configuration}
-                                    poster_path={poster_path} />
+                                <Image
+                                    width={183}
+                                    height={281}
+                                    src={ url }
+                                />
                                 <div className="moviesapp_content">
                                     <div className="content_header">
                                         <h2>{title}</h2>
